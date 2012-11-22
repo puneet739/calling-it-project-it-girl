87b3839d2b6eee88ce3e3b49202d105dc607bfcc
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2012-11-22 08:52:31 +0000

    Adding new functions and removing extra imports.

--- IndianStories/trunk/src/com/stories/marutti/indianstories/entity/Categories.java
+++ IndianStories/trunk/src/com/stories/marutti/indianstories/entity/Categories.java
@@ -1,15 +1,21 @@
 package com.stories.marutti.indianstories.entity;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.stories.marutti.indianstories.details.Log;
+import com.stories.marutti.indianstories.helper.Config;
 
 public class Categories implements Serializable{
 
 	String mTitle;
-	String mImage;
+	transient Bitmap mImage;
 	List<Integer> mStories = new ArrayList<Integer>();
 	
 	public Categories(String Title){
@@ -19,13 +25,19 @@ public class Categories implements Serializable{
 	public String getTitle() {
 		return mTitle;
 	}
+	
 	public void setTitle(String mTitle) {
 		this.mTitle = mTitle;
 	}
-	public String getImage() {
+	
+	public Bitmap getImage() {
+		if (mImage==null){
+			mImage = Config.getInstance().getDefaultImage();
+		}
 		return mImage;
 	}
-	public void setImage(String mImage) {
+	
+	public void setImage(Bitmap mImage) {
 		this.mImage = mImage;
 	}
 
@@ -43,6 +55,11 @@ public class Categories implements Serializable{
 		return details;
 	}
 
+	/**
+	 * Call this to add storyID to the category.
+	 * The passed value should be a number.  
+	 * @param storyId
+	 */
 	public void addStory(String storyId) {
 		try{
 		int StoryID = Integer.parseInt(storyId);
@@ -52,7 +69,27 @@ public class Categories implements Serializable{
 			Log.err("Number Format Exception occured. ");
 			ne.printStackTrace();
 		}
-		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Use this to add some default image to the category view. 
+	 * If nothing is provided, the system will set the default category Image.
+	 * @param imagePath 	Path of the image in the asset folder. 
+	 */
+	public void setImage(String imagePath) {
+		InputStream is = null;
+		Bitmap image = null;
+		try {
+			is = Config.getInstance().getAssetManager().open(imagePath);
+			image = BitmapFactory.decodeStream(is);
+			
+		} catch (FileNotFoundException e) {
+			Log.wrn("Image for Category is not defined/Found in the system. Will use default Image. "+e.getLocalizedMessage());
+		} catch(Exception e){
+			Log.err("Some Different Exception occured. "+e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+		mImage = image;
 		
 	}
 

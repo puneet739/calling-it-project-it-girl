8b9b9cfb72634d01ce3fe34ba27885607a456e6b
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2012-11-29 09:30:34 +0000

    Creating branch for inital development

---
+++ ChildrenStories/branch/V_1_0_0/src/com/stories/marutti/indianstories/BaseActivity.java
@@ -0,0 +1,52 @@
+/**
+ * Nov 24, 2012
+ * puneetb
+ * The file is added by PuneetBehl
+ */
+package com.stories.marutti.indianstories;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * @author puneetb
+ * This is the base class, All the activity of the App should extend this.
+ * In contain basic menu which need to be present everywhere.  
+ * 
+ */
+public class BaseActivity extends Activity{
+
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_aboutdeveloper :
+			createAboutDevDialog();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void createAboutDevDialog() {
+	    Builder builder = new AlertDialog.Builder(this);
+	    builder.setTitle(R.string.about_developer_title);
+	    builder.setMessage(R.string.about_developer_text);
+	    builder.setCancelable(true);
+	    builder.setNeutralButton("OK", null);
+	    AlertDialog dialog = builder.create();
+	    dialog.show();
+	}
+}
---
+++ ChildrenStories/branch/V_1_0_0/src/com/stories/marutti/indianstories/entity/Categories.java
@@ -0,0 +1,96 @@
+package com.stories.marutti.indianstories.entity;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.stories.marutti.indianstories.details.Log;
+import com.stories.marutti.indianstories.helper.Config;
+
+public class Categories implements Serializable{
+
+	String mTitle;
+	transient Bitmap mImage;
+	List<Integer> mStories = new ArrayList<Integer>();
+	
+	public Categories(String Title){
+		this.mTitle = Title;
+	}
+	
+	public String getTitle() {
+		return mTitle;
+	}
+	
+	public void setTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+	
+	public Bitmap getImage() {
+		if (mImage==null){
+			mImage = Config.getInstance().getDefaultImage();
+		}
+		return mImage;
+	}
+	
+	public void setImage(Bitmap mImage) {
+		this.mImage = mImage;
+	}
+
+	public void addStory(Integer storyId){
+		mStories.add(storyId);
+	}
+	
+	public List<Integer> getStories() {
+		return mStories;
+	}
+	
+	@Override
+	public String toString() {
+		String details = "Categories:[Title:"+mTitle+" , mImage:"+mImage+" ,Stories:"+mStories+" ]";
+		return details;
+	}
+
+	/**
+	 * Call this to add storyID to the category.
+	 * The passed value should be a number.  
+	 * @param storyId
+	 */
+	public void addStory(String storyId) {
+		try{
+		int StoryID = Integer.parseInt(storyId);
+		addStory(StoryID);
+		}catch(NumberFormatException ne){
+			Log.err("Story ID cant be a alphanumeric. ");
+			Log.err("Number Format Exception occured. ");
+			ne.printStackTrace();
+		}
+	}
+
+	/**
+	 * Use this to add some default image to the category view. 
+	 * If nothing is provided, the system will set the default category Image.
+	 * @param imagePath 	Path of the image in the asset folder. 
+	 */
+	public void setImage(String imagePath) {
+		InputStream is = null;
+		Bitmap image = null;
+		try {
+			is = Config.getInstance().getAssetManager().open(imagePath);
+			image = BitmapFactory.decodeStream(is);
+			
+		} catch (FileNotFoundException e) {
+			Log.wrn("Image for Category is not defined/Found in the system. Will use default Image. "+e.getLocalizedMessage());
+		} catch(Exception e){
+			Log.err("Some Different Exception occured. "+e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+		mImage = image;
+		
+	}
+
+}

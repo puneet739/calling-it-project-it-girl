1cf2a1ef29713e65e71030f42fa4a96916f6b129
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2012-11-30 11:44:35 +0000

    Merging Branch V1_0_0_0 to trunk

---
+++ ChildrenStories/trunk/src/com/stories/puneet/android/childrenstories/adapters/ProgramInitialingValues.java
@@ -0,0 +1,42 @@
+/**
+ * Nov 23, 2012
+ * 12:33:46 PM
+ * puneetb
+ */
+package com.stories.puneet.android.childrenstories.adapters;
+
+import com.stories.puneet.android.childrenstories.details.Constants;
+import com.stories.puneet.android.childrenstories.details.Log;
+import com.stories.puneet.android.childrenstories.helper.Config;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+/**
+ * @author puneetb
+ *
+ */
+public class ProgramInitialingValues extends AsyncTask<String, Void, Void>{
+
+	Context mContext;
+	public ProgramInitialingValues(Context context) {
+		this.mContext = context;
+	}
+	
+	@Override
+	protected Void doInBackground(String... params) {
+		try{
+			Config mconfig;
+			mconfig = Config.getInstance();
+			mconfig.Inilialize(mContext);
+			mconfig.setStoryStream(mconfig.getAssetManager().open(Constants.FILES.STORY_MAP));
+			mconfig.loadStories();
+			mconfig.setLoaded(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			Log.err("Exception caused while inilizaing values.. Need to check this");
+		}
+		return null;
+	}
+
+}

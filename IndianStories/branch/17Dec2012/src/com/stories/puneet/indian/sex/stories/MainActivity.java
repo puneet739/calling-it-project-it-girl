001d5b37b0eef7b45b7fc0458e63b2944dc86c2a
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-01-16 12:06:04 +0000

    AdWhirl and App Rating added

--- IndianStories/branch/17Dec2012/src/com/stories/puneet/indian/sex/stories/MainActivity.java
+++ IndianStories/branch/17Dec2012/src/com/stories/puneet/indian/sex/stories/MainActivity.java
@@ -1,21 +1,23 @@
 package com.stories.puneet.indian.sex.stories;
 
-import android.app.Activity;
-import android.content.ClipData.Item;
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.RelativeLayout;
 
+import com.adwhirl.AdWhirlLayout;
+import com.adwhirl.AdWhirlLayout.AdWhirlInterface;
+import com.adwhirl.AdWhirlManager;
 import com.stories.puneet.indian.sex.stories.adapters.MyGridAdapter;
 import com.stories.puneet.indian.sex.stories.adapters.ProgramInitialingValues;
 import com.stories.puneet.indian.sex.stories.details.Log;
-import com.stories.puneet.indian.sex.stories.helper.Config;
+import com.stories.puneet.indian.sex.stories.helper.AppRater;
 import com.stories.puneet.indian.sex.stories.listeners.EndlessScrollListener;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements AdWhirlInterface {
 
 	GridView gridview;
 	BaseAdapter madapter;
@@ -29,7 +31,36 @@ public class MainActivity extends BaseActivity {
 		initializeValues();
 		gridview.setAdapter(madapter);
 		
+
+		AppRater.app_launched(this);
+		
+		//AdWhirlConfiguration(this);
 		Log.dbg("Calling On create From MainActivity.");
+	}
+
+	/**
+	 * This program is used to inilialize all the Ads  landing via AdWhirl platform 
+	 * 
+	 * @param mContext the context of the current activity. 
+	 */
+	private void AdWhirlConfiguration(Context  mContext) {
+
+		AdWhirlManager.setConfigExpireTimeout(1000*60*5);
+
+		
+        AdWhirlLayout  adWhirlLayout = (AdWhirlLayout) findViewById(R.id.adwhirl_layout);
+
+        RelativeLayout.LayoutParams layoutParams = new  RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+
+        int diWidth = 320;
+        int diHeight = 52;
+        int density = getResources().getDisplayMetrics().densityDpi;
+        adWhirlLayout.setAdWhirlInterface(this);
+        adWhirlLayout.setMaxWidth((int)(diWidth * density));
+        adWhirlLayout.setMaxHeight((int)(diHeight * density));
+        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+		
 	}
 
 	/**
@@ -55,5 +86,11 @@ public class MainActivity extends BaseActivity {
 	private void LoadBasicValues() {
 		ProgramInitialingValues aSync = new ProgramInitialingValues(getApplicationContext());
 		aSync.execute("Just Calling");
+	}
+
+	@Override
+	public void adWhirlGeneric() {
+		// TODO Auto-generated method stub
+		
 	}
 }

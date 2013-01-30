85d5cb63374a9aa29f6bd6dfdb3cb845254d73ba
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-01-30 16:04:55 +0000

    New Constants

--- IndianStories/branch/17Dec2012/src/com/stories/puneet/indian/sex/stories/details/Constants.java
+++ IndianStories/branch/17Dec2012/src/com/stories/puneet/indian/sex/stories/details/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 	public static final String CATEGORY = "CATEGORY";
 	public static String STORY = "STORY";
 	public static String BUNDLE = "BUNDLE";
+	public static String STORY_YOUTUBE_ID = "STORY_YOUTUBE_ID";
 	
 	public class XMLConstants{
 		//These constants are used from categoriesStories.xml
@@ -23,12 +24,19 @@ public class Constants {
 		public static final String ID="id";
 		public static final String TITLE="title";
 		public static final String FILEPATH="file";
+		public static final String VIDEOID="youtubeid";
 	}
 	
 	public class FILES{
 		public static final String CATEGORY_FILES="XML/categories.xml";
 		public static final String STORY_MAP="XML/allstories.xml";
 		public static final String DEFAULT_IMAGE_PATH = "Images/categories/defaultcategoryimage.jpg";
+	}
+	
+	public class LEADBOLT{
+		public static final String APP_ICON="501285127";
+		public static final String APPWALL="956058664";
+		public static final String INTERSTIAL_CHROME = "252727620";
 	}
 	
 }

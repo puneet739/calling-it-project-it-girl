283cc9273a9c540789a69e9a698b78cf7bd950c1
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-09-24 12:10:50 +0000

    Basic Agent entity

---
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/entity/Agent.java
@@ -0,0 +1,81 @@
+package com.fairdeal.core.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Base entity Object for creating new Agent. 
+ * 
+ * @author puneetb
+ *
+ */
+@Entity
+@Table(name="AGENT")
+public class Agent implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@Column(name="id", nullable=false)
+	private int id;
+	
+	@Column(name="Name")
+	private String name;
+	
+	@Column(name="Email")
+	private String email;
+	
+	@Column(name="CreatedDate")
+	private Date createdDate;
+	
+	@Column(name="PhoneNumber")
+	private String phoneNumber;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+}

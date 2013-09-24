d4f545f6b6517ececa12cead2071ae4b43aeeaf7
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-09-24 12:09:06 +0000

    Added a start just for testing

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/Start.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/Start.java
@@ -1,8 +1,22 @@
 package com.fairdeal.core;
 
+import org.hibernate.Session;
+
+import com.fairdeal.core.db.util.HibernateUtil;
+import com.fairdeal.core.entity.Agent;
+
 public class Start{
 	public static void main(String args[]){
 		System.out.println("Test Project");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		session.beginTransaction();
+		
+		Agent agent = new Agent();
+		agent.setName("Puneet");
+		agent.setPhoneNumber("09971949200");
+		
+		session.save(agent);
+		session.getTransaction().commit();
 	}
-	
 }

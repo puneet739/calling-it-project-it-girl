48a8e25ed9fe355519ad3c276ab8eb5565cb6d88
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-09-26 10:41:53 +0000

    Transaction Factory Test case

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/AgentFactoryTest.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/AgentFactoryTest.java
@@ -5,18 +5,36 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import com.fairdeal.core.db.util.HibernateUtil;
 import com.fairdeal.core.entity.Agent;
+import com.fairdeal.core.factory.AgentFactory;
+import com.fairdeal.core.trans.UserTransaction;
 
 public class AgentFactoryTest {
 
+	@Before
+	public void setup(){
+		//Setting the names of Table which needs to be removed
+		String[] tableNames = {Agent.class.getName()};
+		UserTransaction transaction= new UserTransaction(HibernateUtil.getInstance().getNewSession());
+		
+		//Deleting all the tables at Start
+		for(String tableName: tableNames){
+			Query query = transaction.getSession().createQuery("delete "+tableName);
+			query.executeUpdate();
+		}
+		transaction.getTransaction().commit();
+	}
+	
 	//Basic creation of agent and testing it works fine.
 	@Test
 	public void testAgentEntity(){
-		Session session = HibernateUtil.getSessionFactory().openSession();
+		Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
 		
 		session.beginTransaction();
 		
@@ -37,8 +55,9 @@ public class AgentFactoryTest {
 	//Agent Data from the table as well. in a single Object
 	@Test
 	public void testAgentEntityWithData(){
-		Session session = HibernateUtil.getSessionFactory().openSession();
+		Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
 		
+		//UserTransaction aTrans = new UserTransaction(session.beginTransaction(), session);
 		session.beginTransaction();
 		
 		Agent agent = new Agent();
@@ -53,5 +72,25 @@ public class AgentFactoryTest {
 		
 		assertTrue(list.size()==1);
 	}
+	
+	//In this class we are using AgentFactory for creating the Agent.
+	@Test
+	public void testAgenFactoryCrateAgent(){
+		Session session = HibernateUtil.getInstance().getNewSession();
+		
+		UserTransaction aTrans = new UserTransaction(session);
+		
+		Agent agent = new Agent();
+		agent.setName("Puneet");
+		agent.setPhoneNumber("09971949200");
+		
+		AgentFactory.getInstance().addAgent(aTrans, agent);
+		
+		Query query = session.createQuery("from "+Agent.class.getName());
+		List list = query.list();
+		
+		assertTrue(list.size()==1);
+	}
+
 	
 }

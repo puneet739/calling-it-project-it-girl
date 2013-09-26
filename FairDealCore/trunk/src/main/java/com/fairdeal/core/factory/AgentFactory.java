99f4fe98f7c1c43e210aae5e407e28e3e415a2f8
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-09-26 10:47:16 +0000

    Right now it contain Dummy Details

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/factory/AgentFactory.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/factory/AgentFactory.java
@@ -3,6 +3,7 @@ package com.fairdeal.core.factory;
 import java.util.Map;
 
 import com.fairdeal.core.entity.Agent;
+import com.fairdeal.core.trans.UserTransaction;
 
 /**
  * This will be the default factory which will be dealing with all the CURD operation for 
@@ -11,25 +12,51 @@ import com.fairdeal.core.entity.Agent;
  */
 public class AgentFactory {
 
+	private static AgentFactory mInstance = new AgentFactory(); 
 	
-	
-	public boolean addAgent(Agent agent){	
-		return false;
+	/**
+	 * Providing the Single Instance of AgentFactory. 
+	 */
+	public static AgentFactory getInstance(){
+		return mInstance;
 	}
 	
-	public Agent getAgentbyId(int id){	
+	/**
+	 *  This program will be used add the Agent into the Database. 
+	 */
+	public boolean addAgent(UserTransaction aTrans, Agent agent){	
+		try{
+			aTrans.getSession().save(agent);
+			aTrans.getTransaction().commit();
+		}catch(Exception e){
+			aTrans.getTransaction().rollback();
+			return false;
+		}
+		return true;
+	}
+	
+	public Agent getAgentbyId(UserTransaction aTrans, int id){	
 		return null;
 	}
 	
-	public boolean removeAgent(int id){	
+	public boolean removeAgent(UserTransaction aTrans, int id){	
 		return false;
 	}
 	
-	public Map<String, String> getAgentData(int id){
+	public String getAgentData(int id, String key){
+		//Here we need to create a transaction and close it before finishing this class. 
 		return null;
 	}
 	
-	public boolean updateAgent(int agentId, Agent targetAgent){	
+	public String getAgentData(UserTransaction aTrans, int id, String key){
+		return null;
+	}
+	
+	public Map<String, String> getAgentDatamap(UserTransaction aTrans, int id){
+		return null;
+	}
+	
+	public boolean updateAgent(UserTransaction aTrans, int agentId, Agent targetAgent){	
 		return false;
 	}
 	

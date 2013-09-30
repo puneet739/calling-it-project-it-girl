4c3b8af6c04dc8584e9e1109d16bccf10f134af4
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-09-30 09:41:20 +0000

    Entity refactoring

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/factory/AgentFactory.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/factory/AgentFactory.java
@@ -2,6 +2,10 @@ package com.fairdeal.core.factory;
 
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import com.fairdeal.basic.log4j.Loger;
 import com.fairdeal.core.entity.Agent;
 import com.fairdeal.core.trans.UserTransaction;
 
@@ -23,19 +27,35 @@ public class AgentFactory {
 	
 	/**
 	 *  This program will be used add the Agent into the Database. 
+	 *  @return 		This will return the AgentId of newly Created Agent
 	 */
-	public boolean addAgent(UserTransaction aTrans, Agent agent){	
+	public int addAgent(UserTransaction aTrans, Agent agent){	
 		try{
 			aTrans.getSession().save(agent);
-			aTrans.getTransaction().commit();
 		}catch(Exception e){
 			aTrans.getTransaction().rollback();
-			return false;
+			return 0;
 		}
-		return true;
+		return agent.getId();
 	}
 	
+	/**
+	 * 
+	 * @param aTrans		The Transaction which will be of this session. 
+	 * @param id			The Id of the agent which need to be fetheced.
+	 * @return				The Agent returned from the db of this ID. 
+	 */
 	public Agent getAgentbyId(UserTransaction aTrans, int id){	
+		Agent agent= null;
+		try{
+			agent = (Agent)aTrans.getSession().get(Agent.class, id);
+		}catch(NullPointerException ne){
+			Loger.app.error("NullPointer Exception casued here", ne);
+		}
+		return agent;
+	}
+	
+	public Agent getAgentbyEmail(UserTransaction aTrans, String email){	
 		return null;
 	}
 	

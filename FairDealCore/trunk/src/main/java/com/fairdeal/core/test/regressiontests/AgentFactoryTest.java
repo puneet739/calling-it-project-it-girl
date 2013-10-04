20c28665e35faa4613b10d8d0452f85738327065
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-10-04 13:11:03 +0000

    Adding new test cases

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/AgentFactoryTest.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/AgentFactoryTest.java
@@ -1,6 +1,7 @@
 package com.fairdeal.core.test.regressiontests;
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -101,7 +102,6 @@ public class AgentFactoryTest extends FDTestCase {
 		int id2 = AgentFactory.getInstance().addAgent(aTrans, agent2);
 		int id3 = AgentFactory.getInstance().addAgent(aTrans, agent3);
 		
-		
 		Query query = session.createQuery("from "+Agent.class.getName());
 		List list = query.list();
 		
@@ -109,9 +109,67 @@ public class AgentFactoryTest extends FDTestCase {
 		
 		Agent targetAgent = AgentFactory.getInstance().getAgentbyId(aTrans, id1);
 		aTrans.getTransaction().commit();
-		//aTrans.getTransaction().rollback();
 		assertTrue(targetAgent.equals(agent));
 	}
 
+	@Test
+	public void testAgenFactorygetAgentByEmail(){
+		UserTransaction aTrans = new UserTransaction(session);
+		addAgents();
+		Agent agent = AgentFactory.getInstance().getAgentbyEmail(aTrans, "puneet0@fairdeal.com");
+		assertEquals("puneet0@fairdeal.com", agent.getEmail());
+		assertEquals("Puneet_0", agent.getName());
+		assertEquals("09971949200", agent.getPhoneNumber());
+	}
+	
+	@Test
+	public void testdeleteAgent(){
+		UserTransaction aTrans = new UserTransaction(session);
+
+		Agent agent = new Agent();
+		agent.setName("Puneet");
+		agent.setPhoneNumber("09971949200");
+
+		Agent agent2 = new Agent();
+		agent2.setName("Rahul");
+		agent2.setPhoneNumber("09971949201");
+		
+		Agent agent3 = new Agent();
+		agent3.setName("Puneet2");
+		agent3.setPhoneNumber("09971949202");
+		
+		int id1 = AgentFactory.getInstance().addAgent(aTrans, agent);
+		int id2 = AgentFactory.getInstance().addAgent(aTrans, agent2);
+		int id3 = AgentFactory.getInstance().addAgent(aTrans, agent3);
+		Agent agent_new = (Agent) aTrans.getSession().get(Agent.class, id1);
+		assertNotNull(agent_new);
+		
+		AgentFactory.getInstance().removeAgent(aTrans, id1);
+		
+		agent_new = (Agent) aTrans.getSession().get(Agent.class, id1);
+		assertNull(agent_new);
+		
+		agent_new=(Agent) aTrans.getSession().get(Agent.class, id2);
+		assertNotNull(agent_new);
+		aTrans.getTransaction().commit();
+	}
+	
+	public void addAgents(){
+		Session session = HibernateUtil.getInstance().getNewSession();
+		UserTransaction aTrans = new UserTransaction(session);
+		
+		for (int i=0; i<10; i++){
+			Agent agent = new Agent();
+			agent.setName("Puneet_"+i);
+			agent.setCreatedDate(new Date());
+			agent.setEmail("puneet"+i+"@fairdeal.com");
+			agent.setPhoneNumber("0997194920"+i);
+			AgentFactory.getInstance().addAgent(aTrans, agent);
+		}
+		aTrans.getTransaction().commit();
+		aTrans.getSession().close();
+	}
 	
 }
+
+	

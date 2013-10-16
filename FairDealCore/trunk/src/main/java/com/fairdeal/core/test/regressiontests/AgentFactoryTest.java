4233187ec4ba986c0ce0c5e102c463435b98720a
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-10-16 07:15:52 +0000

    Test Case update

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/AgentFactoryTest.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/AgentFactoryTest.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.StaleStateException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.fairdeal.basic.log4j.Loger;
 import com.fairdeal.core.db.util.HibernateUtil;
 import com.fairdeal.core.entity.Agent;
 import com.fairdeal.core.factory.AgentFactory;
@@ -98,8 +100,8 @@ public class AgentFactoryTest extends FDTestCase {
 		agent3.setPhoneNumber("09971949202");
 		
 		int id1 = AgentFactory.getInstance().addAgent(aTrans, agent);
-		int id2 = AgentFactory.getInstance().addAgent(aTrans, agent2);
-		int id3 = AgentFactory.getInstance().addAgent(aTrans, agent3);
+		AgentFactory.getInstance().addAgent(aTrans, agent2);
+		AgentFactory.getInstance().addAgent(aTrans, agent3);
 		
 		Query query = session.createQuery("from "+Agent.class.getName());
 		List list = query.list();
@@ -153,6 +155,91 @@ public class AgentFactoryTest extends FDTestCase {
 		aTrans.getTransaction().commit();
 	}
 	
+	@Test
+	public void testaddAgentData(){
+		Loger.app.debug("test message");
+		UserTransaction aTrans = new UserTransaction(session);
+		Agent agent = new Agent();
+		agent.setName("Puneet");
+		agent.setPhoneNumber("09971949200");
+		agent.setCreatedDate(new Date());
+		agent.setEmail("puneet739@gmail.com");
+		
+		for (int i=0; i<4; i++){
+			agent.addAgentParam("testKey"+i, "testValue"+i);
+		}
+		
+		int agentId = AgentFactory.getInstance().addAgent(aTrans, agent);
+		
+		aTrans.getTransaction().commit();
+		
+		Agent agent1 = AgentFactory.getInstance().getAgentbyId(aTrans, agentId);
+		
+		for (int i=0; i<4; i++){
+			assertEquals("testValue"+i, agent1.getAgentParam("testKey"+i));
+		}
+	}
+	
+	@Test
+	public void testgetAgentData() {
+		UserTransaction aTrans = new UserTransaction(session);
+		Agent agent = new Agent();
+		agent.setName("Puneet");
+		agent.setPhoneNumber("09971949200");
+		agent.setCreatedDate(new Date());
+		agent.setEmail("puneet739@gmail.com");
+
+		int agentId = AgentFactory.getInstance().addAgent(aTrans, agent);
+
+		for (int i = 0; i < 4; i++) {
+			agent.addAgentParam("testKey" + i, "testValue" + i);
+		}
+		aTrans.getTransaction().commit();
+
+		String value = null;
+
+		value = AgentFactory.getInstance().getAgentData(agentId, "testKey0");
+		assertEquals("testValue0", value);
+
+		value = AgentFactory.getInstance().getAgentData(agentId, "tesssss");
+		assertNull(value);
+	}
+	
+	@Test(expected=StaleStateException.class)
+	public void testSaveUpdatedAgent(){
+		UserTransaction aTrans = new UserTransaction(session);
+		Agent agent = new Agent();
+		agent.setName("Puneet");
+		agent.setPhoneNumber("09971949200");
+		agent.setCreatedDate(new Date());
+		agent.setEmail("puneet739@gmail.com");
+		
+		int agentId = AgentFactory.getInstance().addAgent(aTrans, agent);
+		
+		aTrans.getTransaction().commit();
+		
+		UserTransaction aTrans1= HibernateUtil.getInstance().getUserTransaction(HibernateUtil.getInstance().getNewSession());
+		Agent agent_1 = AgentFactory.getInstance().getAgentbyId(aTrans1, agentId);
+		UserTransaction aTrans2= HibernateUtil.getInstance().getUserTransaction(HibernateUtil.getInstance().getNewSession());
+		Agent agent_2 = AgentFactory.getInstance().getAgentbyId(aTrans2, agentId);
+		
+		agent_1.setEmail("puneet1@gmail.com");
+		AgentFactory.getInstance().addAgent(aTrans1, agent_1);
+		aTrans1.getTransaction().commit();
+		
+		agent_2.setPhoneNumber("9971949202");
+		AgentFactory.getInstance().addAgent(aTrans2, agent_2);
+		aTrans2.getTransaction().commit();
+		
+		Agent finalAgent = AgentFactory.getInstance().getAgentbyId(aTrans2, agentId);
+		assertEquals("puneet1@gmail.com", finalAgent.getEmail());
+		assertEquals("9971949202", finalAgent.getPhoneNumber());
+		
+	}
+	
+	
+	
+	
 	public void addAgents(){
 		Session session = HibernateUtil.getInstance().getNewSession();
 		UserTransaction aTrans = new UserTransaction(session);
@@ -168,6 +255,8 @@ public class AgentFactoryTest extends FDTestCase {
 		aTrans.getTransaction().commit();
 		aTrans.getSession().close();
 	}
+	
+	
 	
 }
 

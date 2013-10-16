a093e0d34e7640f8734600d22ceaf852bbfb8be4
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2013-10-16 07:09:34 +0000

    Updated the new function to delete orphan tables

--- FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/FDTestCase.java
+++ FairDealCore/trunk/src/main/java/com/fairdeal/core/test/regressiontests/FDTestCase.java
@@ -3,6 +3,8 @@
  */
 package com.fairdeal.core.test.regressiontests;
 
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.junit.After;
@@ -10,6 +12,7 @@ import org.junit.Before;
 
 import com.fairdeal.core.db.util.HibernateUtil;
 import com.fairdeal.core.entity.Agent;
+import com.fairdeal.core.entity.AgentData;
 import com.fairdeal.core.trans.UserTransaction;
 
 /**
@@ -24,9 +27,10 @@ public class FDTestCase {
 	public void setup() {
 		session = HibernateUtil.getInstance().getNewSession();
 		// Setting the names of Table which needs to be removed
-		String[] tableNames = { Agent.class.getName() };
+		String[] tableNames = {Agent.class.getName() };
 		UserTransaction transaction = new UserTransaction(HibernateUtil.getInstance().getNewSession());
-
+		
+		deleteOrphanTables();
 		// Deleting all the tables at Start
 		for (String tableName : tableNames) {
 			Query query = transaction.getSession().createQuery(
@@ -36,6 +40,21 @@ public class FDTestCase {
 		transaction.getTransaction().commit();
 	}
 	
+	/**
+	 * This program is used to delete all Orphan tables first the tables. 
+	 */
+	private void deleteOrphanTables() {
+		UserTransaction transaction = new UserTransaction(HibernateUtil.getInstance().getNewSession());
+		String[] tableNames = { AgentData.class.getName()};
+		Session session1 = transaction.getSession();
+		for (String tableName : tableNames) {
+			Query query = session1.createQuery(
+					"delete " + tableName);
+			query.executeUpdate();
+		}
+		transaction.getTransaction().commit();
+	}
+
 	@After
 	public void tearDown() {
 		session.close();
@@ -54,4 +73,5 @@ public class FDTestCase {
 	public void setSession(Session session) {
 		this.session = session;
 	}
+	
 }

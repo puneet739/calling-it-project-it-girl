213ca091fb03c71907232b8b4eac138f217e4846
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2014-06-23 05:33:32 +0000

    Puneetb::: Adding initial release for GSMBug

---
+++ GSMBug/trunk/src/com/fairdeal/android/gsmbug/GSMBug.java
@@ -0,0 +1,17 @@
+package com.fairdeal.android.gsmbug;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GSMBug extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView text = new TextView(this);
+        text.setText("Hello World, Android - mkyong.com");
+        setContentView(text);
+    }
+
+}

0bb7daae93de5767b543f5ab9fa1f9fc476588f2
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2014-06-23 10:26:16 +0000

    Puneetb::: Adding some changes

--- GSMBug/trunk/src/com/fairdeal/android/gsmbug/GSMBug.java
+++ GSMBug/trunk/src/com/fairdeal/android/gsmbug/GSMBug.java
@@ -1,16 +1,44 @@
 package com.fairdeal.android.gsmbug;
 
 import android.app.Activity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class GSMBug extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView text = new TextView(this);
-        text.setText("Hello World, Android - mkyong.com");
-        setContentView(text);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.gsmbuylayout);
+		initializeValue();
+		//loadRecorder();
+	}
+	
+	   private MediaRecorder myAudioRecorder;
+
+
+	private void loadRecorder() {
+		myAudioRecorder = new MediaRecorder();
+		myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+		myAudioRecorder.setOutputFile("testFile.3gp");
+		myAudioRecorder.start();
+	}
+
+	private void initializeValue() {
+		Button button = (Button)findViewById(R.id.androidStartButton);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.println("Some service started");
+			}
+		});
+	}
+
 }

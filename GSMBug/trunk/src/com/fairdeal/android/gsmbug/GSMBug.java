b90209b6edd1ba47ecbc277ec95806ae5cfe711f
puneet739@gmail.com <puneet739@gmail.com@2d79c762-abbf-66f3-b91b-d25263c1d9fb>
2014-07-08 08:18:00 +0000

    Puneetb:: Fixing the calling issue

--- GSMBug/trunk/src/com/fairdeal/android/gsmbug/GSMBug.java
+++ GSMBug/trunk/src/com/fairdeal/android/gsmbug/GSMBug.java
@@ -6,6 +6,7 @@ import com.fairdeal.android.gsmbug.util.LoggerUtil;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,27 +21,32 @@ public class GSMBug extends Activity {
 		setContentView(R.layout.gsmbuylayout);
 		initializeValue();
 	}
-	
-	private MediaRecorder myAudioRecorder;
 
 	private void initializeValue() {
-		Button startButton = (Button)findViewById(R.id.androidStartButton);
+		Button startButton = (Button) findViewById(R.id.androidStartButton);
 		startButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				
+				Intent callingIntent = new Intent();
+				callingIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				callingIntent.setFlags(Intent.FLAG_FROM_BACKGROUND);
+				startService(callingIntent);
 				Intent serviceIntent = new Intent();
-				serviceIntent.setClass(getApplicationContext(), RecorderService.class);
+
+				serviceIntent.setClass(getApplicationContext(),RecorderService.class);
 				startService(serviceIntent);
 				LoggerUtil.debug("RecorderService started");
 			}
 		});
-		
-		Button stopButton = (Button)findViewById(R.id.androidStopButton);
+
+		Button stopButton = (Button) findViewById(R.id.androidStopButton);
 		stopButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent serviceIntent = new Intent();
-				serviceIntent.setClass(getApplicationContext(), RecorderService.class);
+				serviceIntent.setClass(getApplicationContext(),
+						RecorderService.class);
 				stopService(serviceIntent);
 				LoggerUtil.debug("RecorderService Stoped");
 			}
